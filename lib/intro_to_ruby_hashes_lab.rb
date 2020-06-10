336cc9bffca37d6eff5650fffa42b0998b920f7b
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-10 14:23:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,7 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  { :door : "bell"}
+  { :key, "bell"}
+  {:value, 5 }
 
 end
 
